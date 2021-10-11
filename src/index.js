9e10b24f475c34e224ab098378e1f03bf3283069
RihardXXX <yusupa.akaeva@yandex.ru>
2021-10-11 18:01:57 +0300

    setup initial routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from 'routes'
+
+const App = () => {
+    return (
+        <div className="App">
+            <h3>Welcome to react hooks project</h3>
+            <Router>
+                <Routes />
+            </Router>
+        </div>
+    );
+}
 
 ReactDOM.render(
   <React.StrictMode>
